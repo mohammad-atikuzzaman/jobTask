e294e5a35dc9c72162012c1d596b8edfd566f797
Md Atikuzzaman <akash203037@gmail.com>
2025-01-18 13:35:54 +0600

    banner compleated

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -2,8 +2,8 @@ import { AiOutlineMail } from "react-icons/ai";
 
 const Banner = () => {
   return (
-    <section className="flex flex-col gap-4 lg:flex-row justify-between items-center">
-      <div className="bg-emerald-100 flex flex-col-reverse lg:flex-row items-center justify-between p-8 rounded-xl lg:w-[66%]">
+    <section className="flex flex-col gap-4 lg:flex-row justify-between items-stretch">
+      <div className="bg-emerald-100 flex flex-col-reverse lg:flex-row items-center justify-between p-8 rounded-xl lg:w-[66%] shadow-md">
         <div className="space-y-4">
           <h2 className="font-semibold text-4xl text-emerald-800">
             Welcome back 👋 Jaydon Frankie
@@ -16,20 +16,10 @@ const Banner = () => {
             Go Now
           </button>
         </div>
-
         <img src="/analytics.png" alt="" className="w-[30%]" />
       </div>
-      <div className="bg-[url('/background.jpg')] bg-cover bg-center lg:w-[32%] space-y-4 px-4 py-6 rounded-xl">
-      <h2 className="font-semibold text-4xl text-emerald-800">
-            Welcome back 👋 Jaydon Frankie
-          </h2>
-          <p className="text-emerald-500">
-            If you are going to use passage of Lorem Ipusm, you need to be sure
-            thera isnt anything
-          </p>
-          <button className="p-2  rounded-md w-full lg:w-20 bg-emerald-600 text-white font-semibold">
-            Go Now
-          </button>
+      <div className="w-full lg:w-[32%] rounded-xl overflow-hidden shadow-md">
+        <img src="/background.jpg" alt="" className="w-full h-full object-cover" />
       </div>
     </section>
   );
